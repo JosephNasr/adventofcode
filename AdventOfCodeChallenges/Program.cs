e73512ae4cdb37b5d10619c416d41da0bc9750be
Joseph Nasr <Joseph.Nasr@gotocme.com>
2019-01-08 10:45:21 +0200

    Challenge: Day1_Second

---
+++ AdventOfCodeChallenges/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCodeChallenges
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Day1_Second();
+
+            Console.ReadKey(true);
+        }
+
+        private static void Day1_Second()
+        {
+            var inputFrequencies = File.ReadAllLines(@"..\..\Data\day1.txt");
+
+            var frequency = 0;
+            var found = false;
+            var iterations = 0;
+
+            var previousData = new HashSet<int>();
+
+            while (!found)
+            {
+                Console.WriteLine($"Iteration #{iterations++}");
+
+                foreach (var newFrequency in inputFrequencies)
+                {
+                    frequency += Int32.Parse(newFrequency);
+
+                    if (previousData.Contains(frequency))
+                    {
+                        Console.WriteLine($"First duplicate: {frequency}");
+                        found = true;
+                        break;
+                    }
+                    else
+                    {
+                        previousData.Add(frequency);
+                    }
+                }
+            }
+        }
+
+        private static void Day2_First()
+        {
+            var inputIds = File.ReadAllText(@"..\..\Data\day2.txt");
+
+            foreach (var id in inputIds)
+            {
+
+            }
+        }
+    }
+}
